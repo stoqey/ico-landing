6c5100d4bfc605f46721f4fea5ccae8c504a1c95
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-04-21 21:31:23 -0400

    How to invest bar

---
+++ packages/landing/src/containers/AnnouncementBar/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Wrapper } from "./style";
+
+export const AnnouncementBar = () => {
+  const [show, setShow] = useState(true);
+
+  const close = () => {
+    setShow(false);
+  };
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <Wrapper>
+      <div id="hellobar-bar" className="regular closable">
+        <div className="hb-content-wrapper">
+          <div className="hb-text-wrapper">
+            <div className="hb-headline-text">
+              <p>
+                <span>How to invest</span>
+              </p>
+            </div>
+          </div>
+          <a
+            href="https://app.stoqey.com/blog/how-to-invest"
+            target="_blank"
+            className="hb-cta hb-cta-button"
+          >
+            <div className="hb-text-holder">
+              <p>Learn more</p>
+            </div>
+          </a>
+        </div>
+        <div className="hb-close-wrapper">
+          <a href="javascript:void(0);" className="icon-close" onClick={close}>
+            ✖
+          </a>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default AnnouncementBar;
